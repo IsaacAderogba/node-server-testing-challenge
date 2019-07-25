ad2a61d5e0104a8018d6e62b509fd6e3b0fcb126
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-25 12:16:16 +0100

    Create knex migration to add a users table and a foods table

--- data/migrations/20190725120900_createFoodTable.js
+++ data/migrations/20190725120900_createFoodTable.js
@@ -1,8 +1,27 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable("users", users => {
+      users.increments();
+      users
+        .string("username", 128)
+        .notNullable()
+        .unique();
+      users.string("password", 128).notNullable();
+      users.string("department", 128).notNullable();
+    })
+    .createTable("foods", foods => {
+      foods.increments();
+      foods
+        .string("name", 128)
+        .notNullable()
+        .unique();
+      foods.float("proteins").notNullable();
+      foods.float("carbohydrates").notNullable();
+      foods.float("fats").notNullable();
+      foods.float("calories").notNullable();
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("foods").dropTableIfExists("users");
 };
